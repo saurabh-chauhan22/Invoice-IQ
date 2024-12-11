00ff6c72d32dc549615ecbe47be1054536023cac
Saurabh Chauhan <saurabh220398@gmail.com>
2024-12-11 11:37:51 -0600

    Added plot of the spacy transformer model with entities

--- src/python/invoice_iq/invoice_iq/plots.py
+++ src/python/invoice_iq/invoice_iq/plots.py
@@ -1,26 +1,36 @@
+import time
 from pathlib import Path
 
 import typer
-from loguru import logger
-from tqdm import tqdm
 
-from invoice_iq.config import FIGURES_DIR, PROCESSED_DATA_DIR
+from loguru import logger
+
+import spacy
+from spacy import displacy
+
+from invoice_iq.config import FIGURES_DIR, MODEL_ONE_PATH, TEMP_FILE_PATH
 
 app = typer.Typer()
 
 
 @app.command()
-def main(
-    # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    input_path: Path = PROCESSED_DATA_DIR / "dataset.csv",
-    output_path: Path = FIGURES_DIR / "plot.png",
+def main():
+    input_path: Path = TEMP_FILE_PATH
+    output_path: Path = FIGURES_DIR / "entities.xml"
+    model_path: Path = MODEL_ONE_PATH
     # -----------------------------------------
-):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
-    logger.info("Generating plot from data...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
+    logger.info(f"Loading spaCy model from {model_path}...")
+    nlp = spacy.load(model_path)
+    pdf_text = ""
+    with open(input_path,'r') as f:
+        pdf_text = f.read()
+    logger.info("Processing text with spaCy model...")
+    doc = nlp(pdf_text)
+    logger.info("Generating plot ....")
+    time.sleep(2)
+    logger.info("Generating entity visualization in XML format...")
+    xml = displacy.render(doc, style="span")
+    output_path.open("w",encoding="utf-8").write(xml)
     logger.success("Plot generation complete.")
     # -----------------------------------------
 
