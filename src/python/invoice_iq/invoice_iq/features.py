060dd242df7fc42e2c95bbd06e034f55e06cabe0
Saurabh Chauhan <saurabh220398@gmail.com>
2024-12-11 19:11:17 -0600

    Added jinja2 template

--- src/python/invoice_iq/invoice_iq/features.py
+++ src/python/invoice_iq/invoice_iq/features.py
@@ -2,25 +2,18 @@ from pathlib import Path
 
 import typer
 from loguru import logger
-from tqdm import tqdm
-
-from invoice_iq.config import PROCESSED_DATA_DIR
+from jinja2 import Environment, PackageLoader, select_autoescape
 
 app = typer.Typer()
 
 
 @app.command()
-def main(
-    # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    input_path: Path = PROCESSED_DATA_DIR / "dataset.csv",
-    output_path: Path = PROCESSED_DATA_DIR / "features.csv",
-    # -----------------------------------------
-):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
-    logger.info("Generating features from dataset...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
+def main():
+    env = Environment(
+    loader=PackageLoader("invoice_iq"),
+    autoescape=select_autoescape()
+)
+    
     logger.success("Features generation complete.")
     # -----------------------------------------
 
